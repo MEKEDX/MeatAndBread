8326d486ee9a610c13cc81c6c8ae2ed1f547db38
MuhammedEhabAND <muhammedehab726@gmail.com>
2023-10-19 20:41:03 +0300

    adding the timer fragment design

--- app/src/main/java/com/kh/mo/meatandbread/MainActivity.kt
+++ app/src/main/java/com/kh/mo/meatandbread/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.kh.mo.meatandbread
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavController.OnDestinationChangedListener
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
