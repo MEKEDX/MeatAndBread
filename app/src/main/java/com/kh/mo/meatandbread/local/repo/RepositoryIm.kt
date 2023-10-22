c632409877137bc3f53ad81ed6cc7a0e88b70a64
khaledeid1k <khaled.eid1k1k@gmail.com>
2023-10-22 20:35:07 +0300

    finish cart screen

---
+++ app/src/main/java/com/kh/mo/meatandbread/local/repo/RepositoryIm.kt
@@ -0,0 +1,48 @@
+package com.kh.mo.meatandbread.local.repo
+
+import com.kh.mo.meatandbread.local.repo.local.LocalSource
+import com.kh.mo.meatandbread.model.Meal
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+
+class RepositoryIm(private val localSource: LocalSource) : Repository {
+
+    override fun insertMeal(meal: Meal): Completable {
+        return localSource.mealDao.mealDao().insertMeal(meal)
+    }
+
+    override fun updateMeal(meal: Meal): Completable {
+        return localSource.mealDao.mealDao().updateMeal(meal)
+    }
+
+    override fun getAllMeals(): Observable<List<Meal>> {
+        return localSource.mealDao.mealDao().getAllMeals()
+    }
+
+    override fun deleteMeal(meal: Meal): Completable {
+        return localSource.mealDao.mealDao().deleteMeal(meal)
+    }
+    override fun getTotalPrice(): Observable<Int> {
+        return localSource.mealDao.mealDao().getTotalPrice()
+    }
+
+    override fun getHawawshi(): List<Meal> {
+        return localSource.data.listOfHawawshi
+    }
+
+    override fun getMeat(): List<Meal> {
+        return localSource.data.listOfMeat
+    }
+
+
+    override fun getProcessedMeat(): List<Meal> {
+        return localSource.data.listOfProcessedMeat
+    }
+
+
+
+    override fun getMeal(id: Int): Single<Meal> {
+        return localSource.mealDao.mealDao().getPlanedMeal(id)
+    }
+}
--- app/src/main/java/com/kh/mo/meatandbread/model/Meal.kt
+++ app/src/main/java/com/kh/mo/meatandbread/model/Meal.kt
@@ -2,21 +2,37 @@ package com.kh.mo.meatandbread.model
 
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.kh.mo.meatandbread.util.Constants
 
-data class Meal(val id : Int,val image:Int, val name :String?,val mealQuantityRate:Int,
-val price: Int,val mealDetail:String? ,val mealQuantity:String?,
-                val typeofMeal:String?=Constants.meat):Parcelable {
+@Entity("MEALS_TABLE")
+data class Meal(
+    @PrimaryKey(false) val id: Int,
+    val image: Int,
+    val name: String?,
+    val mealQuantityValue: Int,
+    val mealQuantityRate: Int,
+    val price: Int,
+    val mealPriceRate: Int,
+    val mealDetail: String?,
+    val mealQuantity: String?,
+    val typeofMeal: String? = Constants.meat,
+    val time: Int = 0
+) : Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readInt(),
         parcel.readInt(),
         parcel.readString(),
         parcel.readInt(),
         parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt(),
         parcel.readString(),
         parcel.readString(),
-        parcel.readString()
-        )
+        parcel.readString(),
+        parcel.readInt(),
+    )
 
     override fun describeContents(): Int {
         TODO("Not yet implemented")
